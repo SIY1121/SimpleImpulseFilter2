ac1cb5d288285f89c824055ee88a3052b721c69e
SIY1121 <sota.0512.0213@gmail.com>
2018-05-31 18:24:20 +0900

    コメントの追加と最終出力のサンプルを後半に変更

--- src/main/kotlin/main/Controller.kt
+++ src/main/kotlin/main/Controller.kt
@@ -38,18 +38,36 @@ class Controller : Initializable {
     @FXML
     lateinit var irFFTCanvasR: SimpleGraph
 
-    var irSampleL = FloatArray(0)
-    var irSampleR = FloatArray(0)
+    /**
+     * インパルス応答のサンプル L
+     */
+    private var irSampleL = FloatArray(0)
+    /**
+     * インパルス応答のサンプル R
+     */
+    private var irSampleR = FloatArray(0)
 
+    /**
+     * インパルス応答のFFT変換後 L
+     */
     var irFFTL = arrayOf(Complex(0.0))
+    /**
+     * インパルス応答のFFT変換後 R
+     */
     var irFFTR = arrayOf(Complex(0.0))
 
+    /**
+     * 畳み込み先の音声を読み込むグラバー
+     */
     var srcGrabber: FFmpegFrameGrabber? = null
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
 
     }
 
+    /**
+     * インパルス応答選択のボタンが押されたときの処理
+     */
     fun onImpulseSelect(actionEvent: ActionEvent) {
         val file = FileChooser().showOpenDialog(root.scene.window) ?: return
 
@@ -57,6 +75,8 @@ class Controller : Initializable {
         grabber.sampleMode = FrameGrabber.SampleMode.FLOAT
         grabber.start()
 
+        //応答の長さから配列を確保しておく
+        //+1は丸め誤差が発生したときのための保険
         irSampleL = FloatArray((grabber.lengthInTime / 1000_000.0 * grabber.sampleRate).toInt() + 1)
         irSampleR = FloatArray((grabber.lengthInTime / 1000_000.0 * grabber.sampleRate).toInt() + 1)
 
@@ -65,6 +85,8 @@ class Controller : Initializable {
             val buf = grabber.grabSamples()?.samples?.get(0) as? FloatBuffer ?: break
             val tmpArray = FloatArray(buf.limit())
             buf.get(tmpArray)
+
+            //偶数番目のサンプルをL、奇数番目のサンプルをRとして振り分ける
 
             val tmpBufL = tmpArray.filterIndexed { index, _ -> index % 2 == 0 }.toFloatArray()
             irSampleL.replaceRange(read, read + tmpBufL.size, tmpBufL)
@@ -75,25 +97,32 @@ class Controller : Initializable {
             read += buf.limit() / 2
         }
 
+        //サンプルの長さを２の累乗に合わせる
         irSampleL = irSampleL.toPower2()
         irSampleR = irSampleR.toPower2()
 
+        //FFT実行
         val fft = FastFourierTransformer(DftNormalization.STANDARD)
         irFFTL = fft.transform(irSampleL.map { it.toDouble() }.toDoubleArray(), TransformType.FORWARD)
         irFFTR = fft.transform(irSampleR.map { it.toDouble() }.toDoubleArray(), TransformType.FORWARD)
 
+        //グラフに描画
         irSampleCanvasL.data = irSampleL.mapIndexed { index, value -> SimpleGraph.DataPoint(index.toDouble(), value.toDouble()) }
         irSampleCanvasR.data = irSampleR.mapIndexed { index, value -> SimpleGraph.DataPoint(index.toDouble(), value.toDouble()) }
 
         irFFTCanvasL.data = irFFTL.mapIndexed { index, complex -> SimpleGraph.DataPoint(index.toDouble(), 20 * Math.log10(Math.sqrt(complex.real * complex.real + complex.imaginary * complex.imaginary))) }
         irFFTCanvasR.data = irFFTR.mapIndexed { index, complex -> SimpleGraph.DataPoint(index.toDouble(), 20 * Math.log10(Math.sqrt(complex.real * complex.real + complex.imaginary * complex.imaginary))) }
 
+        //Overlap-save法を用いるにあたってサンプルの前半を0でパディングする
         irFFTL = Array(irFFTL.size, { _ -> Complex(0.0) }) + irFFTL
         irFFTR = Array(irFFTR.size, { _ -> Complex(0.0) }) + irFFTR
 
     }
 
     var tmpBuffer: FloatBuffer? = null
+    /**
+     * 指定された数だけ、畳み込み先のサンプルを返す
+     */
     private fun readSamples(size: Int): FloatArray? {
         val result = FloatArray(size)
         var read = 0
@@ -108,12 +137,18 @@ class Controller : Initializable {
         return if (read > 0) result else null
     }
 
+    /**
+     *指定された配列のデータを置き換える
+     */
     fun FloatArray.replaceRange(start: Int, end: Int, replacement: FloatArray) {
         if (end - start != replacement.size) throw Exception("置き換えの配列と範囲の大きさが一致しません")
         for (i in start until end)
             this[i] = replacement[i - start]
     }
 
+    /**
+     * 渡された配列を長さが２の累乗になるようにパディングして返す
+     */
     fun FloatArray.toPower2(): FloatArray {
         var i = 1.0
         while (this.size > Math.pow(2.0, i)) {
@@ -131,6 +166,9 @@ class Controller : Initializable {
         srcGrabber?.start()
     }
 
+    /**
+     * 再生
+     */
     fun play(actionEvent: ActionEvent) {
         Thread({
             val audioFormat = AudioFormat((srcGrabber?.sampleRate?.toFloat() ?: 0f), 16, 2, true, false)
@@ -148,52 +186,60 @@ class Controller : Initializable {
             while (true) {
                 val sample = readSamples(irSampleL.size * 2) ?: break
 
+
+                //読み込んだサンプルをLRに振り分ける
                 val sampleL = sample.filterIndexed { index, _ -> index % 2 == 0 }.toFloatArray()
                 val srcL = prevSampleL + sampleL
 
                 val sampleR = sample.filterIndexed { index, _ -> index % 2 == 1 }.toFloatArray()
                 val srcR = prevSampleR + sampleR
 
+                //FFT変換
                 var fftL = fft.transform(srcL.map { it.toDouble() }.toDoubleArray(), TransformType.FORWARD)
                 var fftR = fft.transform(srcR.map { it.toDouble() }.toDoubleArray(), TransformType.FORWARD)
 
+                //周波数領域で畳み込む
                 fftL = fftL.mapIndexed { index, complex -> irFFTL[index].multiply(complex) }.toTypedArray()
                 fftR = fftR.mapIndexed { index, complex -> irFFTR[index].multiply(complex) }.toTypedArray()
 
+                //逆変換
                 val dstL = fft.transform(fftL, TransformType.INVERSE)
                 val dstR = fft.transform(fftR, TransformType.INVERSE)
 
+                //LRの順でサンプルを格納
                 val dst = FloatArray(dstL.size + dstR.size)
                 for (i in 0 until dstL.size) {
                     dst[i * 2] = dstL[i].real.toFloat()
                     dst[i * 2 + 1] = dstR[i].real.toFloat()
                 }
 
-                val max = dst.max()?:1f
+                //正規化
+                val max = dst.max() ?: 1f
                 println(max)
-                for(i in 0 until dst.size)
-                    dst[i]/=max
+                for (i in 0 until dst.size)
+                    dst[i] /= max
 
+                //shortに変換してバイト配列に変換する
+                //円状畳み込み結果である前半は切り捨て
                 val buf = ByteBuffer.allocate(dst.size).order(ByteOrder.LITTLE_ENDIAN)
                 for (i in 0 until dst.size / 2) {
-                    buf.putShort((dst[i] * Short.MAX_VALUE).toShort())
+                    buf.putShort((dst[i + dst.size / 2] * Short.MAX_VALUE).toShort())
                 }
                 buf.position(0)
 
-                operationQueue.push {
-                    val sL = dstL.mapIndexed { index, complex -> SimpleGraph.DataPoint(index.toDouble(),complex.real) }
-                    val sR = dstR.mapIndexed { index, complex -> SimpleGraph.DataPoint(index.toDouble(),complex.real) }
+                //operationQueue.push {
+                //グラフ描画
+                val sL = dstL.mapIndexed { index, complex -> SimpleGraph.DataPoint(index.toDouble(), complex.real) }
+                val sR = dstR.mapIndexed { index, complex -> SimpleGraph.DataPoint(index.toDouble(), complex.real) }
 
-                    Platform.runLater {
-                        dstCanvasL.data = sL
-                        dstCanvasR.data = sR
-                    }
-
-                    val arr = buf.array()
-                    audioLine.write(arr, 0, arr.size / 4 * 4)
+                Platform.runLater {
+                    dstCanvasL.data = sL
+                    dstCanvasR.data = sR
                 }
 
-
+                val arr = buf.array()
+                audioLine.write(arr, 0, arr.size / 4 * 4)
+                //}
 
 
                 prevSampleL = sampleL
